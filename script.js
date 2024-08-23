1888cae180dbfd3ff1ae5c802fe4721340b6c14c
Alichevictor <alichevictor@gmail.com>
2024-08-22 19:36:12 -0700

    my first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  var open = false;
+
+function openNav() {
+    var sideNav = document.getElementById("mySidenav");
+    
+    if (sideNav.style.width === "0px" || sideNav.style.width === "") {
+        sideNav.style.width = "250px";
+        open = true;
+    } else {
+        sideNav.style.width = "0";
+        open = false;
+    }
+}
+
+  
+function toggleHeight3() {
+  var writeUpsDiv = document.getElementById('droping');
+  var currentHeight = writeUpsDiv.offsetHeight;
+
+  if (currentHeight === 0 ) {
+    writeUpsDiv.style.height = 'auto'; 
+   document.getElementById("inner").innerText="-";
+  } else {
+    writeUpsDiv.style.height = '0px'; 
+    document.getElementById("inner").innerText="+";
+  }
+}
+
+function toggleHeight4() {
+  var writeUpsDiv = document.getElementById('droping1');
+  var currentHeight = writeUpsDiv.offsetHeight;
+
+  if (currentHeight === 0 ) {
+    writeUpsDiv.style.height = 'auto'; 
+   document.getElementById("inner").innerText="-";
+  } else {
+    writeUpsDiv.style.height = '0px'; 
+    document.getElementById("inner").innerText="+";
+  }
+}
+
+
+ 
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var secondDiv = document.getElementById('secondDiv');
+
+    // Listen for scroll events
+    window.addEventListener('scroll', function () {
+        // Check if the user has scrolled down a certain amount (adjust as needed)
+        if (window.scrollY > 250) {
+            // Slide down the sticky div by changing the 'top' property
+            secondDiv.style.top = '0';
+        } else {
+            // Move the sticky div back above the viewport when scrolling up
+            secondDiv.style.top = '-100px';
+        }
+    });
+});
+
+
+const imageUrls = [
+  'images/Rectangle 2.png',
+  'images/pic1.jpg',
+  'images/pic.jpg',
+  'images/pic2.jpg',
+  'images/pic3.jpg',
+  'images/pic5.jpg',
+ 'images/inspection-services.png',
+
+
+
+
+];
+
+let currentIndex = 0;
+
+function updateImage() {
+  const rotatingImage = document.getElementById('rotating-image');
+  rotatingImage.src = imageUrls[currentIndex];
+}
+
+function goToPrevious() {
+  currentIndex = currentIndex === 0 ? imageUrls.length - 1 : currentIndex - 1;
+  updateImage();
+}
+
+function goToNext() {
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+  updateImage();
+}
+
+// Automatically rotate images every 3 seconds
+setInterval(goToNext, 6000);
+
+// Initial image update
+updateImage();
+
+
+
+
